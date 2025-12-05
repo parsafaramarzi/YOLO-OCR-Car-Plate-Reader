d5f24521638507a56f086d646ce479f48544c490
parsafaramarzi <pfaramazi@gmail.com>
2025-12-05 20:49:14 +0330

    update

--- YOLO_OCR_Car_Plate_Reader.py
+++ YOLO_OCR_Car_Plate_Reader.py
@@ -3,10 +3,11 @@ from ultralytics import YOLO
 import easyocr
 from PIL import Image
 import numpy as np
+import re
 
-reader = easyocr.Reader(['en'] , gpu= False, model_storage_directory= 'import')
+reader = easyocr.Reader(['en'] , gpu= True, model_storage_directory= 'import')
 model = YOLO('yolo11n.pt', task = 'detect')
-cap = cv2.VideoCapture('cartraffic02.mp4')
+cap = cv2.VideoCapture('cartraffic01.mp4')
 
 aspect_ratio = cap.get(cv2.CAP_PROP_FRAME_WIDTH) / cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
 new_height = 900
@@ -14,6 +15,19 @@ new_width = int(new_height * aspect_ratio)
 
 plate_numbers = []
 number_of_occurances = {}
+
+def is_valid_plate(concat_number):
+    normalized_plate = concat_number.replace(' ', '').upper()
+    pattern_1 = re.compile(r'^(0[1-9]|[1-7]\d|8[01])[A-Z]{1}\d{2,5}$')
+    pattern_2 = re.compile(r'^(0[1-9]|[1-7]\d|8[01])[A-Z]{2}\d{2,4}$')
+    pattern_3 = re.compile(r'^(0[1-9]|[1-7]\d|8[01])[A-Z]{3}\d{2,3}$')
+    if pattern_1.match(normalized_plate):
+        return True
+    if pattern_2.match(normalized_plate):
+        return True
+    if pattern_3.match(normalized_plate):
+        return True
+    return False
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -28,35 +42,33 @@ while cap.isOpened():
                 coordinates = box.xyxy[0]                        
                 x1, y1, x2, y2 = map(int, coordinates.tolist())
                 cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0), 2)
-                print("the vehicle points are", x1, y1, x2, y2)
                 plate_x1 = int(x1 + (x2 - x1)*15/100)
                 plate_y1 = int(y1 + (y2 - y1)*40/100)
                 plate_x2 = int(x2 - (x2 - x1)*15/100)
                 plate_y2 = int(y2 - (y2 - y1)*10/100)
-                print("the plate points are", plate_x1, plate_y1, plate_x2, plate_y2)
                 cv2.rectangle(frame,(plate_x1,plate_y1),(plate_x2,plate_y2),(255,0,0), 2)
                 plate_region = frame[plate_y1:plate_y2, plate_x1:plate_x2]
                 plate_image = Image.fromarray(cv2.cvtColor(plate_region, cv2.COLOR_RGB2BGR))
                 plate_array = np.array(plate_image)
                 plate_number = reader.readtext(plate_array)
                 concat_number = ' '.join([number[1] for number in plate_number])
-                
-                print(concat_number)
-                cv2.putText(
-                    img = frame,
-                    text = concat_number,
-                    org = (plate_x1, plate_y1 - 10),
-                    fontFace=cv2.FONT_HERSHEY_SIMPLEX,
-                    fontScale=0.7,
-                    color=(0, 0, 255),
-                    thickness=2
-                )
-                if concat_number != '':
-                    plate_numbers.append(concat_number)
-                    if concat_number in number_of_occurances:
-                        number_of_occurances[concat_number] += 1
-                    else:
-                        number_of_occurances[concat_number] = 1
+                if is_valid_plate(concat_number):
+                    print(concat_number)
+                    cv2.putText(
+                        img = frame,
+                        text = concat_number,
+                        org = (plate_x1, plate_y1 - 10),
+                        fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                        fontScale=0.7,
+                        color=(0, 0, 255),
+                        thickness=2
+                    )
+                    if concat_number != '':
+                        plate_numbers.append(concat_number)
+                        if concat_number in number_of_occurances:
+                            number_of_occurances[concat_number] += 1
+                        else:
+                            number_of_occurances[concat_number] = 1
 
     frame = cv2.resize(frame, (new_width, new_height))
 
